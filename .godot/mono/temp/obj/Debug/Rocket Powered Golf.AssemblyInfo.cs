e8681f5b3857c4f82e5246ae5fb5b2879acb5c32
erythrofr <jordane.belot@gmail.com>
2025-01-30 18:37:57 +0100

    better player fps control

--- .godot/mono/temp/obj/Debug/Rocket Powered Golf.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Rocket Powered Golf.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rocket Powered Golf")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4840fa3c45bece57fae977cc703a6c6e7cdb60db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a538ede4009ef5204a8399f77aac421539bcb015")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rocket Powered Golf")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rocket Powered Golf")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
